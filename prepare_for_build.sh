6577a9a10ff9ef423cc2400d7c28575d4c71de2a
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-05-06 15:50:38 -0400

    Remove triton bound

--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -14,18 +14,20 @@ pip --version
 which python
 which pip
 
-## Install libaio
-#echo "Install libaio 0.3.113..."
-#curl https://pagure.io/libaio/archive/libaio-0.3.113/libaio-libaio-0.3.113.tar.gz -o libaio-libaio-0.3.113.tar.gz
-#tar -zxvf libaio-libaio-0.3.113.tar.gz
-#cd /project/libaio-libaio-0.3.113
-#make prefix=/usr install
-#cd /project
+# Download `libaio`.
+echo "Install libaio 0.3.113..."
+curl https://pagure.io/libaio/archive/libaio-0.3.113/libaio-libaio-0.3.113.tar.gz -o libaio-libaio-0.3.113.tar.gz
+tar -zxvf libaio-libaio-0.3.113.tar.gz
+cd /project/libaio-libaio-0.3.113
+make prefix=/usr install
+cd /project
 
-# patch libaio
+# Patch DeepSpeed to statically lijnk `libaio`.
 sed -i "s/'-laio'/'-Wl,-Bstatic', '-laio', '-Wl,-Bdynamic'/g" op_builder/async_io.py
 sed -i "s/'-laio'/'-Wl,-Bstatic', '-laio', '-Wl,-Bdynamic'/g" op_builder/cpu/async_io.py
 
+# Patch DeepSpeed avoid querying `torch` directly.
+#
 # Without this, fails at import time: https://github.com/astral-sh/build-deepspeed/actions/runs/14867513759/job/41747866280?pr=1
 #
 # diff --git a/accelerator/cuda_accelerator.py b/accelerator/cuda_accelerator.py
@@ -43,12 +45,7 @@ sed -i "s/'-laio'/'-Wl,-Bstatic', '-laio', '-Wl,-Bdynamic'/g" op_builder/cpu/asy
 #              return True
 cp build_scripts/cuda_accelerator.py accelerator/cuda_accelerator.py
 
-## force compile comm ops
-#cp op_builder/cpu/comm.py op_builder/comm.py
-#sed -i 's/CPUOpBuilder/TorchCPUOpBuilder/g' op_builder/comm.py
-
-## triton==3.0.0 to support fp_quantizer
-pip install hjson ninja numpy packaging psutil py-cpuinfo pydantic pynvml tqdm libaio deepspeed-kernels "triton>=2.3.0,<=3.0.0"
+pip install hjson ninja numpy packaging psutil py-cpuinfo pydantic pynvml tqdm libaio deepspeed-kernels triton
 
 echo "install torch==${CI_TORCH_VERSION}+cu${TORCH_CUDA_VERSION}"
 pip install --no-cache-dir torch==${CI_TORCH_VERSION} --index-url https://download.pytorch.org/whl/cu${TORCH_CUDA_VERSION}
