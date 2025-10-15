56bfbcce2930b022d6436750a771362d3e166075
William Woodruff <william@astral.sh>
2025-10-15 14:39:04 -0400

    refactor the build matrix
    Signed-off-by: William Woodruff <william@astral.sh>

---
+++ plan-matrix.py
@@ -0,0 +1,115 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "packaging",
+# ]
+# ///
+
+import json
+
+from packaging.version import Version
+
+TORCH_VERSIONS = [
+    # "2.7.0",
+    # "2.8.0",
+    "2.9.0",
+]
+
+PYTHON_VERSIONS = [
+    # "3.9",
+    # "3.10",
+    # "3.11",
+    # "3.12",
+    "3.13",
+    # "3.14",
+]
+
+# Minimum and maximum CUDA versions for each PyTorch version.
+PYTORCH_CUDA_RANGES: dict[str, tuple[str, str]] = {
+    "2.7": ("11.8", "12.8"),
+    "2.9": ("12.6", "13.0"),
+}
+
+# Actual CUDA versions to build against for each PyTorch version.
+PYTORCH_CUDA_VERSIONS: dict[str, list[str]] = {
+    "2.7": ["12.6", "12.8"],
+    "2.9": ["12.6", "12.8", "13.0"],
+}
+
+# CUDA architectures to build against for each (PyTorch version, CUDA version) pair.
+# NOTE(ww): These are probably an overapproximation; I took them from
+# the original unrolled matrix.
+PYTORCH_CUDA_ARCH_LIST: dict[tuple[str, str], str] = {
+    ("2.7", "12.6"): "6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX",
+    ("2.7", "12.8"): "6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0;12.0+PTX",
+    ("2.9", "12.6"): "6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX",
+    ("2.9", "12.8"): "6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0;12.0+PTX",
+    ("2.9", "13.0"): "6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0;10.0;12.0+PTX",
+}
+
+# Matrix exclusions.
+EXCLUSIONS = [
+    # No exclusions yet.
+]
+
+
+def main() -> None:
+    # Every matrix member is a 5-tuple of:
+    # `torch-version`: the PyTorch version as "X.Y.Z", e.g. "2.7.0"
+    # `python-version`: the Python version as "3.X", e.g. "3.10"
+    # `cuda-version`: the CUDA version as "X.Y", e.g. "11.8"
+    # `cuda-arch-list`: the CUDA architectures as a semicolon-separated list
+    # `deepcompile`: 1 or 0, whether to build with DeepCompile support
+
+    rows = []
+    for python_version in PYTHON_VERSIONS:
+        for torch_version in TORCH_VERSIONS:
+            torch_version = Version(torch_version)
+            torch_x_y = f"{torch_version.major}.{torch_version.minor}"
+            cuda_versions = PYTORCH_CUDA_VERSIONS[torch_x_y]
+            for cuda_version in cuda_versions:
+                cuda_arch_list = PYTORCH_CUDA_ARCH_LIST[(torch_x_y, cuda_version)]
+
+                row = {
+                    "torch-version": str(torch_version),
+                    "python-version": python_version,
+                    "cuda-version": cuda_version,
+                    "cuda-arch-list": cuda_arch_list,
+                    # DeepCompile appears to require Torch 2.5 or newer,
+                    # but our original matrix only enabled in on 2.6 and newer.
+                    # Follow that here.
+                    # See: https://github.com/deepspeedai/DeepSpeed/pull/7154
+                    "deepcompile": int(torch_version >= Version("2.6")),
+                }
+
+                if row not in EXCLUSIONS:
+                    rows.append(row)
+
+    # Transform each row to add various nice-to-have representations of fields.
+    for row in rows:
+        # `CI_*` variables: same as the original ones.
+        row["CI_CUDA_VERSION"] = row["cuda-version"]
+        row["CI_TORCH_VERSION"] = row["torch-version"]
+        row["CI_PYTHON_VERSION"] = row["python-version"]
+
+        # `MATRIX_CUDA_VERSION`: same as `cuda-version`, but with the dot removed
+        row["MATRIX_CUDA_VERSION"] = row["cuda-version"].replace(".", "")
+
+        # `MATRIX_TORCH_VERSION`: `torch-version`, but only X.Y, no patch
+        torch_version = Version(row["torch-version"])
+        row["MATRIX_TORCH_VERSION"] = f"{torch_version.major}.{torch_version.minor}"
+
+        # `MATRIX_PYTHON_VERSION`: same as `python-version`, but with the dot removed
+        row["MATRIX_PYTHON_VERSION"] = row["python-version"].replace(".", "")
+
+        # `MANYLINUX_CUDA_VERSION`: same as `cuda-version`
+        row["MANYLINUX_CUDA_VERSION"] = row["cuda-version"]
+
+        # `MANYLINUX_CUDA_COMPAT_VERSION`: `cuda-version`, but in X-Y format instead of X.Y
+        row["MANYLINUX_CUDA_COMPAT_VERSION"] = row["cuda-version"].replace(".", "-")
+
+    print(json.dumps(rows))
+
+
+if __name__ == "__main__":
+    main()
