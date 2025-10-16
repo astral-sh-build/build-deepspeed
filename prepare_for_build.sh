2c4b7bd70b3928777f63a30b9086ea3b97e170d3
William Woodruff <william@astral.sh>
2025-10-16 10:29:36 -0400

    put setuptools in the right place
    Signed-off-by: William Woodruff <william@astral.sh>

--- prepare_for_build.sh
+++ prepare_for_build.sh
@@ -33,7 +33,7 @@ else
     done
 fi
 
-uv pip install hjson ninja numpy packaging psutil py-cpuinfo pydantic pynvml tqdm libaio deepspeed-kernels triton
+uv pip install setuptools hjson ninja numpy packaging psutil py-cpuinfo pydantic pynvml tqdm libaio deepspeed-kernels triton
 
 echo "install torch==${CI_TORCH_VERSION}+cu${TORCH_CUDA_VERSION}"
 uv pip install --no-cache-dir torch==${CI_TORCH_VERSION} --index-url https://download.pytorch.org/whl/cu${TORCH_CUDA_VERSION}
