1c1a48dede5398598114a0188e84fea25d80cfde
Charlie Marsh <charlie.r.marsh@gmail.com>
2025-10-23 18:18:28 -0600

    Update TORCH_CUDA_ARCH_LIST to match build-apex

--- plan-matrix.py
+++ plan-matrix.py
@@ -61,16 +61,16 @@ PYTORCH_CUDA_VERSIONS: dict[tuple[str, str], list[str]] = {
 # CUDA architectures to build against for each PyTorch version.
 TORCH_CUDA_ARCH_LIST = {
     # https://github.com/pytorch/pytorch/blob/134179474539648ba7dee1317959529fbd0e7f89/.ci/manywheel/build_cuda.sh#L55
-    ("2.7", "12.6"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
-    ("2.7", "12.8"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
+    ("2.7", "12.6"): "7.0;7.5;8.0;8.6;9.0+PTX",
+    ("2.7", "12.8"): "7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX",
     # https://github.com/pytorch/pytorch/blob/ba56102387ef21a3b04b357e5b183d48f0afefc7/.ci/manywheel/build_cuda.sh#L56
-    ("2.8", "12.6"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
-    ("2.8", "12.8"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
-    ("2.8", "12.9"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
+    ("2.8", "12.6"): "7.0;7.5;8.0;8.6;9.0+PTX",
+    ("2.8", "12.8"): "7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX",
+    ("2.8", "12.9"): "7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX",
     # https://github.com/pytorch/pytorch/blob/0fabc3ba44823f257e70ce397d989c8de5e362c1/.ci/manywheel/build_cuda.sh#L56
-    ("2.9", "12.6"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
-    ("2.9", "12.8"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
-    ("2.9", "12.9"): "7.5;8.0;8.6;9.0;10.0;12.0+PTX",
+    ("2.9", "12.6"): "7.0;7.5;8.0;8.6;9.0+PTX",
+    ("2.9", "12.8"): "7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX",
+    ("2.9", "12.9"): "7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX",
     ("2.9", "13.0"): "7.5;8.0;8.6;9.0;10.0;11.0;12.0+PTX",
 }
 
@@ -81,7 +81,6 @@ TORCH_GLIBC_VERSION: dict[str, str] = {
     "2.8": "2_27",
     "2.9": "2_27",
 }
-
 
 AUDITWHEEL_BLANKET_EXCLUDES = [
     "libcuda.so",
